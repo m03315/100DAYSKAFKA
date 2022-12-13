baa05d908f2595ec07b6e772f1a3a76af485a7b7
m03315 <38073762+m03315@users.noreply.github.com>
2022-12-13 21:13:04 +0100

    Day 24: kcat

---
+++ kcat/kcat-example.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eu
+
+source ../../../utils/helper.sh
+source ../../../utils/ccloud_library.sh
+
+CONFIG_FILE=~/.confluent/librdkafka.config
+ccloud::validate_ccloud_config $CONFIG_FILE || exit
+
+# Set topic name
+topic_name=test1
+
+# Create topic in Confluent Cloud
+~/bin/confluent kafka topic create --if-not-exists $topic_name
+# Uncomment below if local Kafka cluster
+#kafka-topics --bootstrap-server `grep "^\s*bootstrap.server" $CONFIG_FILE | tail -1` --topic $topic_name --create --if-not-exists
+
+# To specify the configuration file for connecting to the Confluent Cloud cluster
+#  option 1: use `-F <path>` argument (shown in the code below)
+#  option 2: export `KCAT_CONFIG`
+#export KCAT_CONFIG=$CONFIG_FILE
+
+# Produce messages
+num_messages=10
+(for i in `seq 1 $num_messages`; do echo "alice,{\"count\":${i}}" ; done) | \
+   kafkacat -F $CONFIG_FILE \
+        -K , \
+        -P -t $topic_name
+
+# Consume messages
+kafkacat -F $CONFIG_FILE \
+     -K , \
+     -C -t $topic_name -e
