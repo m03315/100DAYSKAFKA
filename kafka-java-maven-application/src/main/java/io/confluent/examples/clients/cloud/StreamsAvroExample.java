83196a767cd2b89a602c28278b759d9879a7b910
m03315 <38073762+m03315@users.noreply.github.com>
2022-12-17 23:24:19 +0100

    Day 28: Avro and Schema Registry: Java

---
+++ kafka-java-maven-application/src/main/java/io/confluent/examples/clients/cloud/StreamsAvroExample.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.confluent.examples.clients.cloud;
+
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+
+import java.util.Map;
+import java.util.Properties;
+
+import static io.confluent.examples.clients.cloud.Util.loadConfig;
+
+public class StreamsAvroExample {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length != 2) {
+          System.out.println("Please provide command line arguments: configPath topic");
+          System.exit(1);
+        }
+    
+        final String topic = args[1];
+    
+        // Load properties from a local configuration file
+        // Create the configuration file (e.g. at '$HOME/.confluent/java.config') with configuration parameters
+        // to connect to your Kafka cluster, which can be on your local host, Confluent Cloud, or any other cluster.
+        // Follow these instructions to create this file: https://docs.confluent.io/platform/current/tutorials/examples/clients/docs/java.html
+        final Properties props = loadConfig(args[0]);
+
+        // Add additional properties.
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "demo-streams-avro-1");
+        // Disable caching to print the aggregation value after each record
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, "-1");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        final Serde<DataRecordAvro> dataRecordAvroSerde = new SpecificAvroSerde<>();
+        final boolean isKeySerde = false;
+        Map<String, String> SRconfig = (Map)props;
+        dataRecordAvroSerde.configure(
+            SRconfig,
+            isKeySerde);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, DataRecordAvro> records = builder.stream(topic, Consumed.with(Serdes.String(), dataRecordAvroSerde));
+
+        KStream<String,Long> counts = records.map((k, v) -> new KeyValue<String, Long>(k, v.getCount()));
+        counts.print(Printed.<String,Long>toSysOut().withLabel("Consumed record"));
+
+        // Aggregate values by key
+        KStream<String,Long> countAgg = counts.groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+            .reduce(
+                (aggValue, newValue) -> aggValue + newValue)
+            .toStream();
+        countAgg.print(Printed.<String,Long>toSysOut().withLabel("Running count"));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+
+        // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+    }
+}
