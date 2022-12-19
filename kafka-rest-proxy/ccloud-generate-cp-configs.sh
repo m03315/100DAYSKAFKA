e9906b0b633ac96eec2557ed989eb6ce99f11f77
m03315 <38073762+m03315@users.noreply.github.com>
2022-12-19 23:14:47 +0100

    Day 30:Avro and Schema Registry: REST Proxy

---
+++ kafka-rest-proxy/ccloud-generate-cp-configs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This file is deprecated in favor of the function in ../utils/ccloud_library.sh
+
+# Source library
+DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source ccloud_library.sh
+
+ccloud::generate_configs "$1"
